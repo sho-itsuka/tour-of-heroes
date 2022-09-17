66b8c1091c235c433b41f35b93bdd19b78fa5d60
sho-itsuka <sho.itsuka122@gmail.com>
2022-09-17 17:07:20 +0900

    add click event

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,11 +8,16 @@ import { HEROES }            from '../mock-heroes'
   styleUrls:   ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+
   heroes = HEROES
+  selectedHero?: Hero
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero
+  }
 }
