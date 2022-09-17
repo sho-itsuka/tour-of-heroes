6d9607339dc850eecb8de268e144b134d42c0270
sho-itsuka <sho.itsuka122@gmail.com>
2022-09-17 19:47:09 +0900

    fixed messages component

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -14,6 +14,7 @@ export class HeroService {
 
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES)
+    this.messageService.add('HeroService: fetched heroes')
     return heroes
   }
 }
