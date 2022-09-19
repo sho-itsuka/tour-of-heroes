50eff8cf40c733380fe65588e901afe9d0884a65
sho-itsuka <sho.itsuka122@gmail.com>
2022-09-19 11:02:00 +0900

    fixed HeroDetailComponent

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Hero } from '../hero';
+// ActivatedRoute インスタンスへのルートに関する情報を保持する
+import { ActivatedRoute }           from '@angular/router';
+// Location 前のビューに戻ることができる
+import { Location }                 from '@angular/common';
+
+import { HeroService } from '../hero.service';
+import { Hero }        from '../hero';
 
 @Component({
   selector:    'app-hero-detail',
@@ -9,9 +15,23 @@ import { Hero } from '../hero';
 export class HeroDetailComponent implements OnInit {
   @Input() hero?: Hero
 
-  constructor() { }
+  constructor(
+    private route:       ActivatedRoute,
+    private heroService: HeroService,
+    private location:    Location
+  ) { }
 
   ngOnInit(): void {
+    this.getHero()
+  }
+  // route.snapshot コンポーネントが作成された直後のルート情報の静的イメージ
+  // paramMap URLから抽出されたルートパラメータ値の辞書
+  // "id"キーは、フェッチするヒーローのidを返す
+  // JSのNumber関数は文字列を数値に変換する(ヒーロのidの値となる)
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'))
+    this.heroService.getHeroes(id)
+      .subscribe(hero => this.hero = hero)
   }
 
 }
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -12,9 +12,10 @@ export class HeroService {
 
   constructor(private messageService: MessageService) { }
 
-  getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES)
-    this.messageService.add('HeroService: fetched heroes')
-    return heroes
+  getHero(id: number): Observable<Hero> {
+    const hero = HEROES.find(h => h.id === id)!
+    // idを埋め込むためのJSのテンプレートリテラルを定義するため、``(バッククォート)を用いる
+    this.messageService.add(`HeroService: fetched hero id=${id}`)
+    return of(hero)
   }
 }
